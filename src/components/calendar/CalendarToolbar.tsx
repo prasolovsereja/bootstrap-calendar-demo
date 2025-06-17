46ea737ffaf07c340643cf6be74d0eb17a16ff68
Сергей Прасолов <prasol555555@gmail.com>
2025-06-17 15:50:04 +0300

    Подключил редьюсеры в компоненты

--- src/components/calendar/CalendarToolbar.tsx
+++ src/components/calendar/CalendarToolbar.tsx
@@ -1,21 +1,45 @@
 import type { FC } from "react";
+import { useAppDispatch, useAppSelector } from "../../slices/hooks";
+import { nextWeek, prevWeek, toggleDisplayMode } from "../../slices/calendarSlice";
+import { format, isEqual } from "date-fns"
+import { ru } from 'date-fns/locale';
 
 const CalendarToolbar: FC = () => {
+  const dispatch = useAppDispatch();
+  const { selectedWeek, currentWeek } = useAppSelector((state) => state.calendar);
+
+
+  const isCurrentWeek = isEqual(selectedWeek.start, currentWeek.start);
+
+  const formattedWeekStart = format(selectedWeek.start, 'dd.MM.yyyy', { locale: ru});
+  const formattedWeekEnd = format(selectedWeek.end, 'dd.MM.yyyy', { locale: ru });
+  
+  const weekRangeStyle = isCurrentWeek ? {color: 'blue' } : {color: 'black' }
+
   return (
     <div
       className="d-flex flex-row calendar-toolbar h-auto vw-100 py-2"
       style={{minWidth: "max-content"}}
     >
       <div className="d-flex flex-row align-items-center justify-content-evenly w-50">
-        <button className="btn btn-outline-primary">{'<-'}</button>
-        <p className="m-0">Date</p>
-        <button className="btn btn-outline-primary">{'->'}</button>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => dispatch(prevWeek())}
+        >{'<-'}</button>
+        <p className="m-0" style={weekRangeStyle} >{`${formattedWeekStart} - ${formattedWeekEnd}`}</p>
+        <button
+          className="btn btn-outline-primary"
+          onClick={() => dispatch(nextWeek())}
+        >{'->'}</button>
       </div>
       <div className="d-flex flex-row justify-content-evenly w-50">
         <button className="btn btn-primary">
           Добавить событие
         </button>
-        <button className="btn btn-primary">Свернуть календарь</button>
+        <button
+          className="btn btn-primary"
+          onClick={() => dispatch(toggleDisplayMode())}  
+        >Свернуть календарь</button>
       </div>
     </div>
   );
